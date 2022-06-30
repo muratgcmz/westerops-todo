0a92a9c61ab50df06bb8841aae8c2a3c2331c199
muratgcmz <gocmezmurat@gmail.com>
2022-06-30 22:42:56 +0300

    project done

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,17 @@
-
 import useSWR, { mutate } from "swr";
 import { useState, useEffect } from "react";
 import Todos from "../components/Todos";
 
-
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function Home() {
   const { data, error } = useSWR("/api/todos", fetcher);
-  const [todoItem, setTodoItem] = useState("");
- 
+  const [todoItem, setTodoItem] = useState(null);
 
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    addTodo(todoItem);
-    setTodoItem("");
-}
-
-  const addTodo = async (title ) => {
+  const addTodo = async (title) => {
     await fetcher("/api/todos", {
       method: "POST",
-      body: JSON.stringify({title: title}),
+      body: JSON.stringify({ title: title }),
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -28,30 +20,27 @@ export default function Home() {
     mutate("/api/todos");
   };
 
-  
+const handleSubmit = (e) => {
+  e.preventDefault();
+ // mutate("/api/todos", [...data, todoItem], false);
+  console.log(todoItem);
+  addTodo(todoItem);
+  setTodoItem("");
+}
 
-  
+
   return (
     
-    <div>
-      <h1 className="text-center font-sans text-[#FFFFFF] text-lg"><strong><span className="text-[#21A7F9]">/</span><span className="text-[#FF7964]">/</span><span className="text-[#00CCDB]">O</span> WesterOps</strong></h1>
-  
-    <div
-      className=" flex w-[718px] h-[800px] 
-      bg-white mt-5 mb-5  flex-col text-gray-200 rounded
-      
-      
-      "
-      id="todoitem"
-    >
     
-      
-      <h1 className="w-36 font-['Inter'] border-b-4 border-[#FF7964] text-center mx-auto text-[#194591] font-semibold text-[20px]">
+    <div
+      className=" flex w-3/5 lg:h-800 md:h-[44rem] sm:h-[25rem] bg-white mt-5 mb-5  flex-col text-gray-200 rounded ">
+      <h1 className="w-36 font-['Inter'] pt-10 border-b-4 border-[#FF7964] text-center mx-auto text-[#194591] font-semibold text-[20px]">
         ToDo List
       </h1>
       <hr />
       <div className="flex flex-col mt-8 w-3/4 mx-auto">
-      <form action="" onSubmit={handleSubmit}>
+      <form action="" 
+      onSubmit={handleSubmit}>
         <div className="flex flex-row justify-center">
           <div className="w-full flex h-10 items-center pl-2 pr-2 mb-2 rounded border-2 border-[#999C9F]">
             <svg
@@ -76,7 +65,6 @@ export default function Home() {
               value={todoItem}
               placeholder="Add a task..."
               onChange={(e) => setTodoItem(e.target.value)}
-            
             />
           </div>
           <button
@@ -88,21 +76,22 @@ export default function Home() {
               className="h-6 w-6"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M14 5l7 7m0 0l-7 7m7-7H3"
               />
             </svg>
           </button>
+          
         </div>
         </form>
 
         <ul>
-        {data?.filter((item) => item.pinned)
-            .map(({ id, title , checked, pinned }) => (
+          {data?.filter((item) => item.pinned)
+            .map(({ id, title , checked,pinned }) => (
               <Todos
               id={id}
               title={title}
@@ -110,9 +99,9 @@ export default function Home() {
               isPinned = {pinned}
               />
             ))},
-          <hr className="border-solid border-sm my-5"></hr>
+          <hr />
           {data?.filter((item) => !item.pinned)
-            .map(({ id, title , checked , pinned}) => (
+            .map(({ id, title , checked , pinned }) => (
               <Todos
               id={id}
               title={title}
@@ -123,6 +112,6 @@ export default function Home() {
         </ul>
       </div>
     </div>
-    </div>
+    
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,43 +8,21 @@ module.exports = {
     
   ],
   theme: {
-    screens: {
-
-      'phone': {'max': '440px'},
-
-      'tablet': {'max': '640px'},
-      // => @media (min-width: 640px) { ... }
-      // 'tablet2': {'max': '740px'},
-
-      'laptop': {'max':'1024px'},
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': {'max':'1280px'},
-      // => @media (min-width: 1280px) { ... }
-    },
+    
     extend: {
       rotate:{
         '270': '270deg',
         '315': '315deg',
       },
-      height:{
-        '800':'800px',
-        '750':'750px',
-        '700': '700px',
-        '600': '600px',
-        '675': '675px',
-        '400': '400px',
-        '200': '200px',
-      }, 
+      // height:{
+      //   '800':'800px',
+        
+      // }, 
 
-      width:{
-        '718':'718px',
-        '600':'600px',
-        '550':'550px',
-        '400':'400px',
-        '300':'300px',
-        '150':'150px',
-      },
+      // width:{
+      //   '718':'718px',
+        
+      // },
      
     },
 
