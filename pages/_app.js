b12de07021d8380338abd738b7b49cbb853234be
muratgcmz <gocmezmurat@gmail.com>
2022-07-03 22:42:49 +0300

    favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-[#85A1BA] to-[#194591] bg-cover"><Component {...pageProps} /></div>
+  return <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-[#85A1BA] to-[#194591] bg-cover"><Component {...pageProps} />
+  <Head>
+        <title>WesterOps Todo App</title>
+        <link rel="icon" href="/favicon.ico" />
+  </Head>
+  
+  </div>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,6 @@ export default function Home() {
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  console.log(todoItem);
   addTodo(todoItem);
   setTodoItem("");
 }
